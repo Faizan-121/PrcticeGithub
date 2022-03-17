5c81fffed2628d88d8c7dc0aa63795db28093302
Faizan-121 <faizan.riasat@devsinc.com>
2022-03-17 22:42:48 +0500

    Adding days last

--- main.rb
+++ main.rb
@@ -2,20 +2,27 @@ $LOAD_PATH << '.'
 require 'linkedList'
 
 l1 = LinkedList.new
-l1.add_at_index(0, 1)
+l1.add_at_head(0)
+l1.add_at_tail(1)
+l1.add_at_tail(2)
+l1.add_at_tail(3)
+l1.add_at_tail(4)
+puts "Length is #{l1.length}"
+l1.display 
+l1.remove_at_index(4)
+puts "Length is #{l1.length}"
+l1.display 
 
-l1.add_at_index(0,0)
+puts "Enter a number:"
+#num = gets
+#puts "Entered thing is:#{num}"
 
-l1.add_at_index(1,2)
-=begin
-l1.add_at_index(2,3)
-l1.add_at_index(2,4)
-=end
-puts "Length is #{l1.length}"
-l1.display 
-l1.pop_head
-puts "Length is #{l1.length}"
-l1.display 
-l1.pop_head
-puts "Length is #{l1.length}"
-l1.display 
+file = File.new("fazi.txt", "r")
+if file
+   # puts "#{file.sysread(100)}"
+else
+    puts "Unable to open file"
+end
+
+arr = IO.readlines("fazi.txt")
+puts arr[5]
